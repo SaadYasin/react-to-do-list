b9fab6bf50f1d3fcc72276077d8f03129ee065b8
SaadYasin <qatar.horizon2009@gmail.com>
2022-11-05 20:57:23 +0300

    first commit

--- .history/src/App_20221105164231.js
+++ .history/src/App_20221105164231.js
@@ -1,9 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold text-red-600 underline">Hello world!</h1>
+      
+      
     </div>
   );
 }
--- .history/src/App_20221105164306.js
+++ .history/src/App_20221105164306.js
@@ -1,9 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold text-red-600 underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
